0d6f81672db53a45b104e5f5063eee3f8ec0fbd9
RookieGamer-Wang <79565691+RookieGamer-Wang@users.noreply.github.com>
2024-04-10 15:19:38 +0800

    Create eval.sh

---
+++ eval.sh
@@ -0,0 +1,71 @@
+# This is an example zero-shot eval script. Please first read the readme_evalharness.md under the same directory.
+
+CHECKPOINT_PATH=/home/Megatron-DeepSpeed/examples/MoE/output/checkpoint/gpt-0.35B-lr-1.2e-4-minlr-1.0e-6-bs-256-gpus-8-mp-1-pp-1-ep-32-mlc-0.01-cap-1.0-drop-true/global_step30/
+CONFIG_PATH=/home/Megatron-DeepSpeed/examples/MoE/ds_config_gpt_gpt-0.35B-lr-1.2e-4-minlr-1.0e-6-bs-256-gpus-64-mp-1-pp-1-ep-128-mlc-0.01-cap-1.0-drop-true.json
+RESULT_PATH=gpt_gpt-0.35B-lr-1.2e-4-minlr-1.0e-6-bs-256-gpus-64-mp-1-pp-1-ep-128-mlc-0.01-cap-1.0-drop-true.log
+
+PP_SIZE=1
+TP_SIZE=1
+NO_PP="true"
+EP_PARALLEL_SIZE=4
+# Currently eval harness does not support data parallel
+# However, for MoE models it's possible to enable a "fake data parallel"
+# in order to load experts on multiple gpus. At the same time, it's not
+# real data parallel because we load the same data on all gpus.
+# On the other hand, it's better to use less number of gpus than training,
+# to reduce communication overhead.p
+NUM_NODE=1
+NUM_GPU_PER_NODE=8
+
+TASKS="lambada_openai"
+# WikiText-2, not used in GPT-3 paper but used in GPT-2 paper
+# TASKS="wikitext"
+# Tasks that appeared in GPT-3 paper (sorted based on the order in paper), plus WikiText-2.
+# TASKS="hellaswag,lambada,triviaqa,webqs,winogrande,piqa,arc_challenge,arc_easy,openbookqa,race,boolq,cb,copa,rte,wic,wsc,multirc,record,anli_r1,anli_r2,anli_r3,wikitext"
+# All tasks that confirmed to work, there are more tasks on https://github.com/EleutherAI/lm-evaluation-harness that we didn't test.
+# TASKS="hellaswag,lambada,triviaqa,webqs,winogrande,piqa,arc_challenge,arc_easy,openbookqa,race,boolq,cb,copa,rte,wic,wsc,multirc,record,anli_r1,anli_r2,anli_r3,wikitext,logiqa,mathqa,mc_taco,mrpc,prost,pubmedqa,qnli,qqp,sciq,sst,wnli"
+
+VOCAB_FILE=/home/data/Megatron-LM/the_pile_public_merged_nopreprocessing/gpt2-vocab.json
+MERGE_FILE=/home/data/Megatron-LM/the_pile_public_merged_nopreprocessing/gpt2-merges.txt
+
+export HF_DATASETS_OFFLINE=1
+
+# Dummy arguments to make megatron happy. No need to configure them.
+# The reason we don't need to configure them and many other arguments is
+# because the eval framework will read the arguments from checkpoint file.
+MEGATRON_REQUIRED_ARGS="\
+    --num-layers -1\
+    --hidden-size -1\
+    --num-attention-heads -1\
+    --seq-length -1 \
+    --max-position-embeddings -1
+"
+
+CMD="../../tasks/eval_harness/evaluate.py \
+    --load $CHECKPOINT_PATH\
+    --tensor-model-parallel-size $TP_SIZE \
+    --pipeline-model-parallel-size $PP_SIZE\
+    --moe-expert-parallel-size ${EP_PARALLEL_SIZE} \
+    --vocab-file $VOCAB_FILE\
+    --merge-file $MERGE_FILE\
+    --micro-batch-size 12\
+    --no-load-optim \
+    --no-load-rng \
+    --inference \
+    --disable-moe-token-dropping \
+    --adaptive_seq_len\
+    --eval_fp32\
+    --task_list $TASKS\
+    --results_path $RESULT_PATH \
+    --deepspeed \
+    --deepspeed_config $CONFIG_PATH \
+    $MEGATRON_REQUIRED_ARGS\
+    "
+
+if [[ "${NO_PP}" = "true" ]]; then
+CMD="${CMD} \
+    --no-pipeline-parallel"
+fi
+
+LAUNCHER="deepspeed --num_nodes $NUM_NODE --num_gpus $NUM_GPU_PER_NODE "
+$LAUNCHER $CMD &> eval.log
